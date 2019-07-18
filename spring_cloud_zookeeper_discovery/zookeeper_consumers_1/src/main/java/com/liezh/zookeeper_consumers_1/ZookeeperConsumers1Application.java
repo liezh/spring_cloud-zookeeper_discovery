514dfb3f2e386c329afd0b179c9e82c8b906870d
liezh <13556024285@163.com>
2019-07-18 17:05:22 +0800

    zookeeper 服务消费者

---
+++ spring_cloud_zookeeper_discovery/zookeeper_consumers_1/src/main/java/com/liezh/zookeeper_consumers_1/ZookeeperConsumers1Application.java
@@ -0,0 +1,13 @@
+package com.liezh.zookeeper_consumers_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperConsumers1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperConsumers1Application.class, args);
+    }
+
+}
---
+++ spring_cloud_zookeeper_discovery/zookeeper_consumers_1/src/test/java/com/liezh/zookeeper_consumers_1/ZookeeperConsumers1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liezh.zookeeper_consumers_1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZookeeperConsumers1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
